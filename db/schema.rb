2516cc74a95a9ca05a51ad37a0ab1e1a067e1301
Molly <mollycoleiro@gmail.com>
2021-10-14 15:14:55 +0100

    added ambiance and SEO

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_08_135738) do
+ActiveRecord::Schema.define(version: 2021_10_14_135348) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2021_10_08_135738) do
     t.string "cuisine"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "phone_number"
+    t.string "ambiance"
   end
 
 end
